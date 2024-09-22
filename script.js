b63f81ff705b57078281674c2b29deeae31c84fc
Amy Reichhold <a.reichhold99@gmail.com>
2024-09-21 21:57:28 -0700

    Added dynamic navigation bar showing/hiding when scrolling.

---
+++ script.js
@@ -0,0 +1,29 @@
+let lastScrollTop = 0;
+
+function keepNavbarVisible() {
+    const navbar = document.getElementById('navbar');
+    navbar.style.top = '0';
+}
+
+function scrollChangesNavbarVisibility() {
+    const navbar = document.getElementById('navbar');
+
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+      // Scrolling down
+      navbar.style.top = '-3em'; // Hide the navbar (adjust based on navbar height)
+    } else {
+      // Scrolling up
+      navbar.style.top = '0';
+    }
+    
+    lastScrollTop = scrollTop;
+}
+
+
+function main() {
+    window.addEventListener('scroll', scrollChangesNavbarVisibility);
+}
+
+window.onload = main;
